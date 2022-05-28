618f099f4d6a236dbdb1912a3320de5e44ffd7af
risako070310 <risako070310@gmail.com>
2022-05-28 09:08:13 +0900

    use JavaScript Channel

--- lib/src/web_view_stack.dart
+++ lib/src/web_view_stack.dart
@@ -54,6 +54,7 @@ class _WebViewStackState extends State<WebViewStack> {
             return NavigationDecision.navigate;
           },
           javascriptMode: JavascriptMode.unrestricted,
+          javascriptChannels: _createJavascriptChannels(context),
         ),
         if (loadingPercentage < 100)
           LinearProgressIndicator(
@@ -61,5 +62,16 @@ class _WebViewStackState extends State<WebViewStack> {
           )
       ],
     );
+  }
+
+  Set<JavascriptChannel> _createJavascriptChannels(BuildContext context) {
+    return {
+      JavascriptChannel(
+        name: 'SnackBar',
+        onMessageReceived: (message) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message.message)));
+        },
+      ),
+    };
   }
 }
